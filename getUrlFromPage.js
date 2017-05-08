f37a901e7096ae4f0558b4f2a8ab587dda2d2ec8
Gautam Hathi <gkh8@duke.edu>
2017-05-08 18:53:52 -0400

    updating to use ground-truth 0.2.0

--- getUrlFromPage.js
+++ getUrlFromPage.js
@@ -1,7 +1,7 @@
 pageUrl = window.location.href;
 
-if (pageUrl.indexOf("google.com") >= 0) {
-  chrome.runtime.sendMessage({currentUrl: pageUrl}, function(response) {
-      console.log("response recieved");
-  });
-}
+
+chrome.runtime.sendMessage({currentUrl: pageUrl}, function(response) {
+    console.log("response recieved");
+});
+
--- traceStory.js
+++ traceStory.js
@@ -2,9 +2,5 @@ var gt = require('ground-truth');
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-
-    if (request.currentUrl.indexOf("google.com") >= 0) {
-      gt.findSourceReporting('http://thehill.com/homenews/administration/323411-trump-transition-team-was-told-flynn-would-likely-need-to-register-as', 0.5, 15, function(link) {console.log("RESULT: " + link)});  
-    }  
-
+      gt.findSourceReporting(request.currentUrl, 0.3, 15, function(result, path) {console.log("RESULT: " + JSON.stringify(path))});  
 });
