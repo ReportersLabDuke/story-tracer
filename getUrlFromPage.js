6a8060ee27f35a79576215c9e89bed3ccc58319a
Gautam Hathi <gkh8@duke.edu>
2017-05-12 00:00:54 -0400

    trying to make sure that dialog box is fully removed when closed

--- getUrlFromPage.js
+++ getUrlFromPage.js
@@ -3,10 +3,11 @@ pageUrl = window.location.href;
 
 chrome.runtime.sendMessage({currentUrl: pageUrl}, function(response) {
   if (response[0].score > 0.3) {
-    document.body.innerHTML += '<dialog>' + "This page may be based on content from: " + "<a href='response[0].source'>" + response[0].source + "</a>" + '<br><button>Close</button></dialog>';
+    document.body.innerHTML += "<dialog>" + "This page may be based on content from: " + "<a href='" + response[0].source + "'>" + response[0].source + "</a> <br><button>Close</button></dialog>";
     var dialog = document.querySelector("dialog");
     dialog.querySelector("button").addEventListener("click", function() {
       dialog.close();
+      dialog.parentNode.removeChild(dialog);
     });
     dialog.style = "position: fixed; width: 400px; height: auto; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: hidden; text-align: center; top: 25px; left: calc(100% - 425px);";
     dialog.show();
