a8b8b9546f3edf6a602e961d6bcb8ccde79c4b20
Gautam Hathi <gkh8@duke.edu>
2017-05-24 17:38:32 -0700

    moving more sources display trigger to button

--- getUrlFromPage.js
+++ getUrlFromPage.js
@@ -6,14 +6,22 @@ chrome.runtime.sendMessage({currentUrl: pageUrl}, function(response) {
     var n = document.createElement("dialog");
     document.body.appendChild(n);
     var dialog = document.querySelector("dialog")
-    dialog.innerHTML += "This page may be based on content from: " + createSourceLink(response[0][0]) + "<br><button>Close</button>";
+    dialog.innerHTML += "This page may be based on content from: " + createSourceLink(response[0][0]) + "<br><button id='close'>Close</button><button id='more'>More sources...</button>";
     if (response[0].length > 1) {
-      var listDiv = "<div class='sourcelist'><p>Hover here to see more sources: </p>" + createLinkList(response[0].slice(1)) + "</div>";
+      var listDiv = "<div class='sourcelist'>" + createLinkList(response[0].slice(1)) + "</div>";
       dialog.innerHTML += listDiv;
     }
-    dialog.querySelector("button").addEventListener("click", function() {
+    dialog.querySelector("#close").addEventListener("click", function() {
       dialog.close();
       dialog.parentNode.removeChild(dialog);
+    });
+    dialog.querySelector("#more").addEventListener("click", function () {
+      var moreSources = document.querySelector(".sourcelist");
+      if (moreSources.style.height !== "auto") {
+        moreSources.style.height = "auto";
+      } else {
+        moreSources.style.height = "";
+      }      
     });
     dialog.style = "position: fixed; width: 400px; height: auto; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: hidden; text-align: center; top: 25px; left: calc(100% - 425px);";
     dialog.show();
