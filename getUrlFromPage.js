cc828df55decd7e0906a51774a31c7c126e6140f
Gautam Hathi <gautamhathi@live.com>
2017-10-29 23:18:24 -0700

    adding excluded sites list to prevent google.com from making trouble

--- getUrlFromPage.js
+++ getUrlFromPage.js
@@ -1,40 +1,46 @@
 pageUrl = window.location.href;
 
+var excluded_urls = [
+  "google.com",
+  "youtube.com"
+]
 
-chrome.runtime.sendMessage({currentUrl: pageUrl}, function(response) {
-  if (response[0][0].score > 0.3) {
-    var n = document.createElement("dialog");
-    document.body.appendChild(n);
-    var dialog = document.querySelector("dialog")
-    dialog.innerHTML += "This page may be based on content from: " + createSourceLink(response[0][0]) + "<br><button id='close'>Close</button><button id='more'>More sources...</button>";
-    if (response[0].length > 1) {
-      var listDiv = "<div class='sourcelist'>" + createLinkList(response[0].slice(1)) + "</div>";
-      dialog.innerHTML += listDiv;
-    } else {
-      var moreButton = dialog.querySelector("#more");
-      moreButton.style.display = "none";
-    }
-    dialog.querySelector("#close").addEventListener("click", function() {
-      dialog.close();
-      dialog.parentNode.removeChild(dialog);
-    });
-    dialog.querySelector("#more").addEventListener("click", function () {
-      var moreSources = document.querySelector(".sourcelist");
-      if (moreSources.style.height !== "auto") {
-        moreSources.style.height = "auto";
-        moreSources.style.overflowY = "scroll";
-        moreSources.style.maxHeight = "500px";
+if (!checkExcludedUrls(pageUrl, excluded_urls)) {
+  chrome.runtime.sendMessage({ currentUrl: pageUrl }, function (response) {
+    if (response[0][0].score > 0.3) {
+      var n = document.createElement("dialog");
+      document.body.appendChild(n);
+      var dialog = document.querySelector("dialog")
+      dialog.innerHTML += "This page may be based on content from: " + createSourceLink(response[0][0]) + "<br><button id='close'>Close</button><button id='more'>More sources...</button>";
+      if (response[0].length > 1) {
+        var listDiv = "<div class='sourcelist'>" + createLinkList(response[0].slice(1)) + "</div>";
+        dialog.innerHTML += listDiv;
       } else {
-        moreSources.style.height = "";
-        moreSources.style.overflowY = "";
-        moreSources.style.maxHeight = "";
-      }      
-    });
-    dialog.style = "position: fixed; width: 400px; height: auto; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 150000000030; overflow: hidden; text-align: center; top: 25px; left: calc(100% - 425px);";
-    dialog.show();
-  }  
-  console.log("RESULT RESPONSE: " + JSON.stringify(response));
-});
+        var moreButton = dialog.querySelector("#more");
+        moreButton.style.display = "none";
+      }
+      dialog.querySelector("#close").addEventListener("click", function () {
+        dialog.close();
+        dialog.parentNode.removeChild(dialog);
+      });
+      dialog.querySelector("#more").addEventListener("click", function () {
+        var moreSources = document.querySelector(".sourcelist");
+        if (moreSources.style.height !== "auto") {
+          moreSources.style.height = "auto";
+          moreSources.style.overflowY = "scroll";
+          moreSources.style.maxHeight = "500px";
+        } else {
+          moreSources.style.height = "";
+          moreSources.style.overflowY = "";
+          moreSources.style.maxHeight = "";
+        }
+      });
+      dialog.style = "position: fixed; width: 400px; height: auto; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 150000000030; overflow: hidden; text-align: center; top: 25px; left: calc(100% - 425px);";
+      dialog.show();
+    }
+    console.log("RESULT RESPONSE: " + JSON.stringify(response));
+  });
+}
 
 function createSourceLink (sourceObject) {
   return "<a href='" + sourceObject.url + "'>" + sourceObject.url + "</a>"; 
@@ -49,4 +55,14 @@ function createLinkList (linkArray) {
   }
   html += "</ul>";
   return html;
+}
+
+function checkExcludedUrls(currentUrl, excludedList) {
+  for (i = 0; i < excludedList.length; i++) {
+    if (currentUrl.indexOf(excludedList[i]) >= 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
